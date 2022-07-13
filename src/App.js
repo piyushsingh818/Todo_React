c2b7a99f5211fb0063fe9c8bfc8923cf06dcf04a
Piyush <Piyush.Singh@khtechnologies.in>
2022-07-13 07:27:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react'
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 function App() {
+  const [tasks,setTasks]= useState( [
+    {
+        id: 1,
+        text: 'Doctors Appointment1',
+        day: 'Feb 5th at 2:30pm ',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Doctors Appointment2',
+        day: 'Feb 5th at 3:30pm ',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Doctors Appointment3',
+        day: 'Feb 5th at 7:30pm ',
+        reminder: false,
+    },
+])
+// Delete tasks
+const deleteTasks = (id) =>
+  setTasks(tasks.filter((task)=>task.id !== id
+))
+ 
+// Toggle tasks
+const toggleTasks =(id)=>{
+ setTasks(tasks.map((task)=>
+ task.id === id ? 
+ {...task, reminder: !task.reminder}  : task))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTasks} onToggle={toggleTasks}/>
+     : 'No Tasks'
+    }
+       
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from './Button'
+import  PropTypes from 'prop-types'
+const Header = ({ title }) => {
+    const onClick = (e) => {
+        console.log('click');
+    }
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+           <Button  onClick={onClick}color='green' text="Add" />
+        </header>
+    )
+}
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+export default Header
